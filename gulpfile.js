1f58d6b1eb936091c4a1937b738d3d32a7cb56c6
yokselzok <yokselzok@gmail.com>
2017-01-26 20:10:23 +0300

    Add plumber

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var minifyCss = require('gulp-minify-css');
 var browserSync = require('browser-sync');
+var plumber = require('gulp-plumber');
 var reload = browserSync.reload;
 
 
 gulp.task('sass', function() {
   return sass('src/scss/**/styles.scss')
+    .pipe(plumber())
     .pipe(minifyCss())
     .pipe(gulp.dest('assets/css'))
 });
