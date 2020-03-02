093a80d9b5b2d15c06df5e04ef3389cb2a63acd6
yoksel <Yuliya.Buhvalova@rambler-co.ru>
2020-03-02 13:50:43 +0300

    Beautify gulp actions

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,41 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var browserSync = require('browser-sync').create();
-var plumber = require('gulp-plumber');
 var reload = browserSync.reload;
 var sass = require('gulp-sass');
+var copy = require('gulp-copy');
+var del = require('del');
 
 sass.compiler = require('node-sass');
 
-gulp.task('sass', function() {
-  return gulp.src('src/scss/**/styles.scss')
+gulp.task('sass', function () {
+  return gulp.src(['src/scss/**/styles.scss'])
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
     .pipe(gulp.dest('assets/css'))
     .pipe(browserSync.stream());
 });
 
-gulp.task('serve', function() {
+gulp.task('js', gulp.series( function() {
+  return gulp.src(['src/**/*.js'])
+    .pipe(copy('assets', { prefix: 1 }));
+}));
+
+gulp.task('del', function () {
+  return del([
+    'build'
+  ]);
+});
+
+gulp.task('build', gulp.series(['del', 'sass', 'js'], function() {
+  return gulp.src(['assets/**/*', './index.html'])
+    .pipe(copy('build/'));
+}));
+
+gulp.task('serve', gulp.series(['sass', 'js'], function() {
   browserSync.init({
     server: {
-        baseDir: '.'
+      baseDir: '.'
     }
   });
 
@@ -28,6 +45,11 @@ gulp.task('serve', function() {
   );
 
   gulp.watch(
+    ['src/js/**/*'],
+    gulp.series(['js'])
+  );
+
+  gulp.watch(
     ['*.html', 'parser/**/*', 'assets/css/**/*.css', 'assets/js/**/*.js']
   ).on('change', reload);
-});
+}));
