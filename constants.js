2fffc6039df128faf83b7a3c11911960f388cfbb
yoksel <yokselzok@gmail.com>
2020-06-22 13:57:46 +0300

    Add counter

--- constants.js
+++ constants.js
@@ -3,7 +3,7 @@ const DESC = 'Demos for W3C CSS Flexbox Specification http://www.w3.org/TR/css-f
 const URL = 'https://yoksel.github.io/flex-cheatsheet/';
 const IMAGE = '';
 const GITHUB = 'https://github.com/yoksel/flex-cheatsheet/';
-const COUNTER = ``;
+const COUNTER = '<!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(65071600, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, trackHash:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/65071600" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->';
 const HIGHLIGHT_GRID = 0;
 
 module.exports = {
