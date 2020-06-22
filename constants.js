ddfef56182728d99d9d3ead683a33b0fd07eb0ad
yoksel <yokselzok@gmail.com>
2020-06-22 14:31:48 +0300

    Add actual screenshoot; update readme

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 const TITLE = 'Flex Cheatsheet';
 const DESC = 'Demos for W3C CSS Flexbox Specification http://www.w3.org/TR/css-flexbox-1/';
 const URL = 'https://yoksel.github.io/flex-cheatsheet/';
-const IMAGE = '';
+const IMAGE = '//yoksel.github.io/flex-cheatsheet/screenshot.png';
 const GITHUB = 'https://github.com/yoksel/flex-cheatsheet/';
 const COUNTER = '<!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(65071600, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, trackHash:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/65071600" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->';
 const HIGHLIGHT_GRID = 0;
--- readme.md
+++ readme.md
@@ -1,9 +1,12 @@
 # Flexbox cheatsheet
 
-http://yoksel.github.io/flex-cheatsheet/
-
 The list of Flexbox properties with:
 
 * live demos
 * code snippets
 * definitions from the [W3C Flexbox specification](https://www.w3.org/TR/css-flexbox-1/)
+
+http://yoksel.github.io/flex-cheatsheet/
+
+<img src="https://yoksel.github.io/flex-cheatsheet/screenshot.png"
+alt="Preview of Flex Cheatsheet" width="500"/>
